3f267a81c125bbaf33278b8f544c2bd29282ee1f
Talha <64659821+talhaozbek61@users.noreply.github.com>
2024-06-06 15:52:19 +0300

    added yup validation and fixed bugs

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -5,87 +5,8 @@ import { Label } from "@/components/ui/label";
 import { Textarea } from "@/components/ui/textarea";
 import { useState } from "react";
 import { useForm } from "react-hook-form";
-
-export default function page() {
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm();
-  const [data, setData] = useState();
-  const onSubmit = (data) => {
-    setData(data);
-  };
-  return (
-    <div className="mx-auto max-w-2xl py-8 lg:py-12">
-      {data ? (
-        <Response data={data} />
-      ) : (
-        <>
-          <h1 className="text-3xl font-semibold text-center">Contact</h1>
-          <form className="py-4 space-y-2" onSubmit={handleSubmit(onSubmit)}>
-            <div>
-              <Label htmlFor="name" className={errors.name && "text-red-500"}>
-                {errors.name ? "Please Enter Name" : "Name"}
-              </Label>
-              <Input
-                type="text"
-                id="name"
-                className="rounded-sm focus-visible:ring-0"
-                {...register("name", {
-                  required: true,
-                })}
-              />
-            </div>
-            <div>
-              <Label htmlFor="email" className={errors.email && "text-red-500"}>
-                {errors.email ? "Please Enter Email" : "Email"}
-              </Label>
-              <Input
-                type="text"
-                id="email"
-                className="rounded-sm focus-visible:ring-0"
-                {...register("email", {
-                  pattern:
-                    /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,
-                  required: true,
-                })}
-              />
-            </div>
-            <div>
-              <Label htmlFor="phone" className={errors.phone && "text-red-500"}>
-                {errors.phone ? "Please Enter Phone Number" : "Phone"}
-              </Label>
-              <Input
-                type="text"
-                id="phone"
-                className="rounded-sm focus-visible:ring-0"
-                {...register("phone", { required: true })}
-              />
-            </div>
-            <div>
-              <Label
-                htmlFor="message"
-                className={errors.message && "text-red-500"}
-              >
-                {errors.message ? "Please Enter Message" : "Message"}
-              </Label>
-              <Textarea
-                id="message"
-                placeholder="Type your message here."
-                className="rounded-sm focus-visible:ring-0"
-                {...register("message", {
-                  required: true,
-                })}
-              />
-            </div>
-            <Button className="w-full">Send</Button>
-          </form>
-        </>
-      )}
-    </div>
-  );
-}
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as Yup from "yup";
 
 const Response = ({ data }) => {
   return (
@@ -106,3 +27,127 @@ const Response = ({ data }) => {
     </>
   );
 };
+
+export default function Page() {
+  // Schema
+  const validationSchema = Yup.object().shape({
+    name: Yup.string()
+      .min(2, "Enter your full name.")
+      .required("Name is required."),
+    email: Yup.string()
+      .email("Invalid e-mail address.")
+      .required("E-mail address is required."),
+    phone: Yup.number()
+      .typeError("Invalid phone number.")
+      .required("Telephone number is required."),
+    message: Yup.string().required("Message is required."),
+  });
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isValid },
+    reset,
+  } = useForm({
+    resolver: yupResolver(validationSchema),
+    mode: "onChange",
+  });
+  const [data, setData] = useState(null);
+  const onSubmit = async (data) => {
+    setData(await data);
+    reset();
+  };
+  return (
+    <div className="mx-auto max-w-2xl py-8 lg:py-12">
+      {data ? (
+        <Response data={data} />
+      ) : (
+        <>
+          <h1 className="text-3xl font-semibold text-center">Contact</h1>
+          <form
+            className="py-4 space-y-2 mx-auto max-w-xl"
+            onSubmit={handleSubmit(onSubmit)}
+          >
+            <div>
+              <Label
+                htmlFor="name"
+                className={`block text-sm font-medium ${
+                  errors.name ? "text-red-400" : "text-gray-700"
+                } `}
+              >
+                {errors.name ? errors.name?.message : "Name"}
+              </Label>
+              <Input
+                type="text"
+                id="name"
+                className="rounded-sm focus-visible:ring-0 mt-1"
+                placeholder="Name"
+                {...register("name", {
+                  required: true,
+                })}
+              />
+            </div>
+            <div>
+              <Label
+                htmlFor="email"
+                className={`block text-sm font-medium ${
+                  errors.email ? "text-red-400" : "text-gray-700"
+                } `}
+              >
+                {errors.email ? errors.email?.message : "Email"}
+              </Label>
+              <Input
+                type="text"
+                id="email"
+                className="rounded-sm focus-visible:ring-0 mt-1"
+                placeholder="Email"
+                {...register("email", {
+                  pattern:
+                    /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,
+                  required: true,
+                })}
+              />
+            </div>
+            <div>
+              <Label
+                htmlFor="phone"
+                className={`block text-sm font-medium ${
+                  errors.phone ? "text-red-400" : "text-gray-700"
+                } `}
+              >
+                {errors.phone ? errors.phone?.message : "Phone"}
+              </Label>
+              <Input
+                type="text"
+                id="phone"
+                className="rounded-sm focus-visible:ring-0 mt-1"
+                placeholder="Phone"
+                {...register("phone", { required: true })}
+              />
+            </div>
+            <div>
+              <Label
+                htmlFor="message"
+                className={`block text-sm font-medium ${
+                  errors.message ? "text-red-400" : "text-gray-700"
+                } `}
+              >
+                {errors.message ? errors.message?.message : "Message"}
+              </Label>
+              <Textarea
+                id="message"
+                placeholder="Message"
+                className="rounded-sm focus-visible:ring-0 mt-1"
+                {...register("message", {
+                  required: true,
+                })}
+              />
+            </div>
+            <Button className="w-full" disabled={!isValid}>
+              Send
+            </Button>
+          </form>
+        </>
+      )}
+    </div>
+  );
+}
